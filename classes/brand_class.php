a583897da4664600d473759dd905da4b32717396
Lady-Margaret (Marge) Hagan <144915421+ladymargehagan@users.noreply.github.com>
2025-10-26 19:50:29 +0000

    i3

--- classes/brand_class.php
+++ classes/brand_class.php
@@ -6,15 +6,31 @@ class brand_class extends db_connection
     public function add_brand($brand_name, $brand_image = '')
     {
         // Check if brand name already exists
-        $check_sql = "SELECT brand_id FROM brands WHERE brand_name = '$brand_name'";
-        if ($this->db_fetch_one($check_sql)) {
+        $check_sql = "SELECT brand_id FROM brands WHERE brand_name = ?";
+        $stmt = $this->db->prepare($check_sql);
+        $stmt->bind_param("s", $brand_name);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        if ($result->num_rows > 0) {
+            $stmt->close();
             return false;
         }
+        $stmt->close();
 
-        $sql = "INSERT INTO brands (brand_name, brand_image) VALUES ('$brand_name', '$brand_image')";
-        $result = $this->db_write_query($sql);
+        $sql = "INSERT INTO brands (brand_name, brand_image) VALUES (?, ?)";
+        $stmt = $this->db->prepare($sql);
+        $stmt->bind_param("ss", $brand_name, $brand_image);
+        $result = $stmt->execute();
         
-        return $result;
+        if ($result) {
+            // Return the ID of the inserted brand
+            $insert_id = $this->db->insert_id;
+            $stmt->close();
+            return $insert_id;
+        }
+        
+        $stmt->close();
+        return false;
     }
 
     public function get_brands_by_user($user_id)
@@ -35,30 +51,50 @@ class brand_class extends db_connection
 
     public function get_brand_by_id($brand_id)
     {
-        $sql = "SELECT * FROM brands WHERE brand_id = '$brand_id'";
-        $result = $this->db_fetch_one($sql);
+        $sql = "SELECT * FROM brands WHERE brand_id = ?";
+        $stmt = $this->db->prepare($sql);
+        $stmt->bind_param("i", $brand_id);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $brand = $result->fetch_assoc();
+        $stmt->close();
         
-        return $result;
+        return $brand;
     }
 
     public function update_brand($brand_id, $brand_name, $brand_image = '')
     {
         // Check if brand exists
-        $check_sql = "SELECT brand_id FROM brands WHERE brand_id = '$brand_id'";
-        $brand_exists = $this->db_fetch_one($check_sql);
+        $check_sql = "SELECT brand_id FROM brands WHERE brand_id = ?";
+        $stmt = $this->db->prepare($check_sql);
+        $stmt->bind_param("i", $brand_id);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $brand_exists = $result->fetch_assoc();
+        $stmt->close();
         
         if (!$brand_exists) {
             return false;
         }
 
         // Check if new name already exists (excluding current brand)
-        $check_name_sql = "SELECT brand_id FROM brands WHERE brand_name = '$brand_name' AND brand_id != '$brand_id'";
-        if ($this->db_fetch_one($check_name_sql)) {
+        $check_name_sql = "SELECT brand_id FROM brands WHERE brand_name = ? AND brand_id != ?";
+        $stmt = $this->db->prepare($check_name_sql);
+        $stmt->bind_param("si", $brand_name, $brand_id);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $name_exists = $result->fetch_assoc();
+        $stmt->close();
+        
+        if ($name_exists) {
             return false;
         }
 
-        $sql = "UPDATE brands SET brand_name = '$brand_name', brand_image = '$brand_image' WHERE brand_id = '$brand_id'";
-        $result = $this->db_write_query($sql);
+        $sql = "UPDATE brands SET brand_name = ?, brand_image = ? WHERE brand_id = ?";
+        $stmt = $this->db->prepare($sql);
+        $stmt->bind_param("ssi", $brand_name, $brand_image, $brand_id);
+        $result = $stmt->execute();
+        $stmt->close();
         
         return $result;
     }
@@ -66,21 +102,36 @@ class brand_class extends db_connection
     public function delete_brand($brand_id)
     {
         // Check if brand exists
-        $check_sql = "SELECT brand_id FROM brands WHERE brand_id = '$brand_id'";
-        $brand_exists = $this->db_fetch_one($check_sql);
+        $check_sql = "SELECT brand_id FROM brands WHERE brand_id = ?";
+        $stmt = $this->db->prepare($check_sql);
+        $stmt->bind_param("i", $brand_id);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $brand_exists = $result->fetch_assoc();
+        $stmt->close();
         
         if (!$brand_exists) {
             return false;
         }
 
         // Check if brand is used in products
-        $check_products_sql = "SELECT product_id FROM products WHERE product_brand = '$brand_id'";
-        if ($this->db_fetch_one($check_products_sql)) {
+        $check_products_sql = "SELECT product_id FROM products WHERE product_brand = ?";
+        $stmt = $this->db->prepare($check_products_sql);
+        $stmt->bind_param("i", $brand_id);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $product_exists = $result->fetch_assoc();
+        $stmt->close();
+        
+        if ($product_exists) {
             return false;
         }
 
-        $sql = "DELETE FROM brands WHERE brand_id = '$brand_id'";
-        $result = $this->db_write_query($sql);
+        $sql = "DELETE FROM brands WHERE brand_id = ?";
+        $stmt = $this->db->prepare($sql);
+        $stmt->bind_param("i", $brand_id);
+        $result = $stmt->execute();
+        $stmt->close();
         
         return $result;
     }
@@ -100,11 +151,23 @@ class brand_class extends db_connection
 
     public function brand_name_exists($brand_name, $exclude_id = null)
     {
-        $sql = "SELECT brand_id FROM brands WHERE brand_name = '$brand_name'";
+        $sql = "SELECT brand_id FROM brands WHERE brand_name = ?";
         if ($exclude_id) {
-            $sql .= " AND brand_id != '$exclude_id'";
+            $sql .= " AND brand_id != ?";
         }
-        return $this->db_fetch_one($sql) ? true : false;
+        
+        $stmt = $this->db->prepare($sql);
+        if ($exclude_id) {
+            $stmt->bind_param("si", $brand_name, $exclude_id);
+        } else {
+            $stmt->bind_param("s", $brand_name);
+        }
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $exists = $result->fetch_assoc();
+        $stmt->close();
+        
+        return $exists ? true : false;
     }
 }
 ?>
--- classes/category_class.php
+++ classes/category_class.php
@@ -5,115 +5,207 @@ class category_class extends db_connection
 {
     public function add_category($cat_name, $created_by, $cat_image = '')
     {
-        $check_sql = "SELECT cat_id FROM categories WHERE cat_name = '$cat_name'";
-        if ($this->db_fetch_one($check_sql)) {
+        $check_sql = "SELECT cat_id FROM categories WHERE cat_name = ?";
+        $stmt = $this->db->prepare($check_sql);
+        $stmt->bind_param("s", $cat_name);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        if ($result->num_rows > 0) {
+            $stmt->close();
             return false;
         }
+        $stmt->close();
 
-        $sql = "INSERT INTO categories (cat_name, created_by, cat_image) VALUES ('$cat_name', '$created_by', '$cat_image')";
-        $result = $this->db_write_query($sql);
+        $sql = "INSERT INTO categories (cat_name, created_by, cat_image) VALUES (?, ?, ?)";
+        $stmt = $this->db->prepare($sql);
+        $stmt->bind_param("sis", $cat_name, $created_by, $cat_image);
+        $result = $stmt->execute();
         
         if (!$result) {
-            $sql = "INSERT INTO categories (cat_name, cat_image) VALUES ('$cat_name', '$cat_image')";
-            $result = $this->db_write_query($sql);
+            $sql = "INSERT INTO categories (cat_name, cat_image) VALUES (?, ?)";
+            $stmt = $this->db->prepare($sql);
+            $stmt->bind_param("ss", $cat_name, $cat_image);
+            $result = $stmt->execute();
         }
         
-        return true;
+        if ($result) {
+            $insert_id = $this->db->insert_id;
+            $stmt->close();
+            return $insert_id;
+        }
+        
+        $stmt->close();
+        return false;
     }
 
     public function get_categories_by_user($user_id)
     {
-        $sql = "SELECT * FROM categories WHERE created_by = '$user_id' ORDER BY cat_name ASC";
-        $result = $this->db_fetch_all($sql);
+        $sql = "SELECT * FROM categories WHERE created_by = ? ORDER BY cat_name ASC";
+        $stmt = $this->db->prepare($sql);
+        $stmt->bind_param("i", $user_id);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $categories = array();
+        while ($row = $result->fetch_assoc()) {
+            $categories[] = $row;
+        }
+        $stmt->close();
         
-        if ($result === false || empty($result)) {
+        if (empty($categories)) {
             $sql = "SELECT * FROM categories ORDER BY cat_name ASC";
             $result = $this->db_fetch_all($sql);
+            return $result ? $result : array();
         }
         
-        return $result;
+        return $categories;
     }
 
     public function get_category_by_id($cat_id, $user_id)
     {
-        $sql = "SELECT * FROM categories WHERE cat_id = '$cat_id' AND created_by = '$user_id'";
-        $result = $this->db_fetch_one($sql);
+        $sql = "SELECT * FROM categories WHERE cat_id = ? AND created_by = ?";
+        $stmt = $this->db->prepare($sql);
+        $stmt->bind_param("ii", $cat_id, $user_id);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $category = $result->fetch_assoc();
+        $stmt->close();
         
-        if (!$result) {
-            $sql = "SELECT * FROM categories WHERE cat_id = '$cat_id'";
-            $result = $this->db_fetch_one($sql);
+        if (!$category) {
+            $sql = "SELECT * FROM categories WHERE cat_id = ?";
+            $stmt = $this->db->prepare($sql);
+            $stmt->bind_param("i", $cat_id);
+            $stmt->execute();
+            $result = $stmt->get_result();
+            $category = $result->fetch_assoc();
+            $stmt->close();
         }
         
-        return $result;
+        return $category;
     }
 
     public function update_category($cat_id, $cat_name, $user_id, $cat_image = '')
     {
-        $check_sql = "SELECT cat_id FROM categories WHERE cat_id = '$cat_id' AND created_by = '$user_id'";
-        $category_exists = $this->db_fetch_one($check_sql);
+        $check_sql = "SELECT cat_id FROM categories WHERE cat_id = ? AND created_by = ?";
+        $stmt = $this->db->prepare($check_sql);
+        $stmt->bind_param("ii", $cat_id, $user_id);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $category_exists = $result->fetch_assoc();
+        $stmt->close();
         
         if (!$category_exists) {
-            $check_sql = "SELECT cat_id FROM categories WHERE cat_id = '$cat_id'";
-            $category_exists = $this->db_fetch_one($check_sql);
+            $check_sql = "SELECT cat_id FROM categories WHERE cat_id = ?";
+            $stmt = $this->db->prepare($check_sql);
+            $stmt->bind_param("i", $cat_id);
+            $stmt->execute();
+            $result = $stmt->get_result();
+            $category_exists = $result->fetch_assoc();
+            $stmt->close();
         }
         
         if (!$category_exists) {
             return false;
         }
 
-        $check_name_sql = "SELECT cat_id FROM categories WHERE cat_name = '$cat_name' AND cat_id != '$cat_id'";
-        if ($this->db_fetch_one($check_name_sql)) {
+        $check_name_sql = "SELECT cat_id FROM categories WHERE cat_name = ? AND cat_id != ?";
+        $stmt = $this->db->prepare($check_name_sql);
+        $stmt->bind_param("si", $cat_name, $cat_id);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $name_exists = $result->fetch_assoc();
+        $stmt->close();
+        
+        if ($name_exists) {
             return false;
         }
 
-        $sql = "UPDATE categories SET cat_name = '$cat_name', cat_image = '$cat_image' WHERE cat_id = '$cat_id' AND created_by = '$user_id'";
-        $result = $this->db_write_query($sql);
+        $sql = "UPDATE categories SET cat_name = ?, cat_image = ? WHERE cat_id = ? AND created_by = ?";
+        $stmt = $this->db->prepare($sql);
+        $stmt->bind_param("ssii", $cat_name, $cat_image, $cat_id, $user_id);
+        $result = $stmt->execute();
         
         if (!$result) {
-            $sql = "UPDATE categories SET cat_name = '$cat_name', cat_image = '$cat_image' WHERE cat_id = '$cat_id'";
-            $result = $this->db_write_query($sql);
+            $sql = "UPDATE categories SET cat_name = ?, cat_image = ? WHERE cat_id = ?";
+            $stmt = $this->db->prepare($sql);
+            $stmt->bind_param("ssi", $cat_name, $cat_image, $cat_id);
+            $result = $stmt->execute();
         }
         
+        $stmt->close();
         return $result;
     }
 
     public function delete_category($cat_id, $user_id)
     {
-        $check_sql = "SELECT cat_id FROM categories WHERE cat_id = '$cat_id' AND created_by = '$user_id'";
-        $category_exists = $this->db_fetch_one($check_sql);
+        $check_sql = "SELECT cat_id FROM categories WHERE cat_id = ? AND created_by = ?";
+        $stmt = $this->db->prepare($check_sql);
+        $stmt->bind_param("ii", $cat_id, $user_id);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $category_exists = $result->fetch_assoc();
+        $stmt->close();
         
         if (!$category_exists) {
-            $check_sql = "SELECT cat_id FROM categories WHERE cat_id = '$cat_id'";
-            $category_exists = $this->db_fetch_one($check_sql);
+            $check_sql = "SELECT cat_id FROM categories WHERE cat_id = ?";
+            $stmt = $this->db->prepare($check_sql);
+            $stmt->bind_param("i", $cat_id);
+            $stmt->execute();
+            $result = $stmt->get_result();
+            $category_exists = $result->fetch_assoc();
+            $stmt->close();
         }
         
         if (!$category_exists) {
             return false;
         }
 
-        $check_products_sql = "SELECT product_id FROM products WHERE product_cat = '$cat_id'";
-        if ($this->db_fetch_one($check_products_sql)) {
+        $check_products_sql = "SELECT product_id FROM products WHERE product_cat = ?";
+        $stmt = $this->db->prepare($check_products_sql);
+        $stmt->bind_param("i", $cat_id);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $product_exists = $result->fetch_assoc();
+        $stmt->close();
+        
+        if ($product_exists) {
             return false;
         }
 
-        $sql = "DELETE FROM categories WHERE cat_id = '$cat_id' AND created_by = '$user_id'";
-        $result = $this->db_write_query($sql);
+        $sql = "DELETE FROM categories WHERE cat_id = ? AND created_by = ?";
+        $stmt = $this->db->prepare($sql);
+        $stmt->bind_param("ii", $cat_id, $user_id);
+        $result = $stmt->execute();
         
         if (!$result) {
-            $sql = "DELETE FROM categories WHERE cat_id = '$cat_id'";
-            $result = $this->db_write_query($sql);
+            $sql = "DELETE FROM categories WHERE cat_id = ?";
+            $stmt = $this->db->prepare($sql);
+            $stmt->bind_param("i", $cat_id);
+            $result = $stmt->execute();
         }
         
+        $stmt->close();
         return $result;
     }
 
     public function category_name_exists($cat_name, $exclude_id = null)
     {
-        $sql = "SELECT cat_id FROM categories WHERE cat_name = '$cat_name'";
+        $sql = "SELECT cat_id FROM categories WHERE cat_name = ?";
         if ($exclude_id) {
-            $sql .= " AND cat_id != '$exclude_id'";
+            $sql .= " AND cat_id != ?";
         }
-        return $this->db_fetch_one($sql) ? true : false;
+        
+        $stmt = $this->db->prepare($sql);
+        if ($exclude_id) {
+            $stmt->bind_param("si", $cat_name, $exclude_id);
+        } else {
+            $stmt->bind_param("s", $cat_name);
+        }
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $exists = $result->fetch_assoc();
+        $stmt->close();
+        
+        return $exists ? true : false;
     }
 }
 ?>
